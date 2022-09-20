9e1b6a2f95e3f2ff23fbda36ee744687967c35d9
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:33:36 -0500

    Add files via upload

---
+++ Meno get y set/Meno get y set/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meno_get_y_set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //agraga el constructor
+            menoclass men = new menoclass();
+            men.N11=int.Parse(textBox1.Text);
+            men.N21 = int.Parse(textBox2.Text);
+
+            textBox3.Text = men.meno().ToString();
+        }
+    }
+}
---
+++ Meno get y set/Meno get y set/meno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meno_get_y_set
+{
+    internal class menoclass
+    {
+        
+        private int N1;
+        private int N2;
+
+        public int N11 { get => N1; set => N1 = value; }
+        public int N21 { get => N2; set => N2 = value; }
+
+        //metodo, accion, lo que hace.
+        public int meno()
+        {
+            return (N1 - N2);
+        }
+    }
+}
